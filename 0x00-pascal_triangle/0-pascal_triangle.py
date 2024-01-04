d2c3c892b7abf26f82bc81e3228bf8d761f342c6
lawrence <lodhiambo26@gmail.com>
2024-01-04 12:54:57 +0000

    pythagoras triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1]
+        if triangle:
+            last_row = triangle[-1]
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            row.append(1)
+        triangle.append(row)
+
+    return triangle
