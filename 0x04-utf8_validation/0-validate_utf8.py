e9dd8818665714e78b72fcaa1092aa25cd9a6536
lawrence <lodhiambo26@gmail.com>
2024-02-02 09:32:06 +0000

    0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,3 +14,32 @@ def validUTF8(data):
     Each integer represents 1 byte of data, therefore you only need
         to handle the 8 least significant bits of each integer
     '''
+    def isStartByte(byte):
+        '''
+        checks for start
+        '''
+        return (byte & 0b10000000) == 0b00000000
+
+    def isContinuationByte(byte):
+        '''
+        checks if byte is concurrent
+        '''
+        return (byte & 0b11000000) == 0b10000000
+    i = 0
+    while i < len(data):
+        num_bytes = 1
+        mask = 0b10000000
+        while data[i] & mask:
+            num_bytes += 1
+            mask >>= 1
+
+        if num_bytes < 1 or num_bytes > 4:
+            return False
+
+        for j in range(1, num_bytes):
+            if i + j >= len(data) or not isContinuationByte(data[i + j]):
+                return False
+
+        i += num_bytes
+
+    return True
